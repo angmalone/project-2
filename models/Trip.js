c5d94c56ddcb777bec170f5e5537c54747747ac5
Angela Malone <a.malone1193@gmail.com>
2018-03-05 09:54:28 -0500

    start model construction

---
+++ models/Trip.js
@@ -0,0 +1,33 @@
+//connect to database
+const mongoose = require("../db/connection");
+
+//create schema
+const TripSchema = new mongoose.Schema({
+  name: String,
+  restaurants: [{ type: Schema.Types.ObjectId, ref: "Restaurant" }],
+  places: [{ type: Schema.Types.ObjectId, ref: "Place" }]
+});
+
+const RestaurantSchema = new mongoose.Schema({
+  name: String,
+  website: String,
+  type: String,
+  eat: String,
+  notes: String
+});
+
+const PlaceSchema = new mongoose.Schema({
+  name: String,
+  website: String,
+  notes: String
+});
+
+// Giving mongoose the schema to build the model
+const Trip = mongoose.model("Trip", TripSchema);
+const Restaurant = mongoose.model("Restaurant", RestaurantSchema);
+const Place = mongoose.model("Place", PlaceSchema);
+
+// exporting the Joke model
+module.exports = Trip;
+module.exports = Restaurant;
+module.exports = Place;

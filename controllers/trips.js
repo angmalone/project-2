10274f7c439caec3f3e036562f23d668e381d663
Angela Malone <a.malone1193@gmail.com>
2018-03-08 07:40:52 -0500

    cleanup

--- controllers/trips.js
+++ controllers/trips.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const Trip = require("../models/Trip");
 var sortJsonArray = require("sort-json-array");
 
-//load individual trips
 router.get("/", (req, res) => {
   Trip.find({}).then(trips => {
     sortJsonArray(trips, "location", "asc");
@@ -12,19 +11,16 @@ router.get("/", (req, res) => {
   });
 });
 
-//create a new trip
 router.get("/new", (req, res) => {
   res.render("trips/new");
 });
 
-//load details of each individual trip
 router.get("/:id", (req, res) => {
   Trip.findOne({ _id: req.params.id }).then(trips => {
     res.render("trips/show", trips);
   });
 });
 
-//editing the details
 router.get("/edit/:id", (req, res) => {
   Trip.findOne({ _id: req.params.id }).then(trips => {
     res.render("trips/edit", trips);
@@ -39,14 +35,12 @@ router.put("/:id", (req, res) => {
   );
 });
 
-//deleting a trip
 router.delete("/:id", (req, res) => {
   Trip.findOneAndRemove({ _id: req.params.id }).then(() => {
     res.redirect("/trips");
   });
 });
 
-//creating the new trip
 router.post("/", (req, res) => {
   Trip.create({
     location: req.body.location,
